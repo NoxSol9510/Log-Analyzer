d0ec8a05cd73bab0382946f76adcf018e56285e6
NoxSol9510 <114277558+NoxSol9510@users.noreply.github.com>
2025-08-20 15:25:42 +0700

    Intergrate log level count to the menu

--- main.py
+++ main.py
@@ -1,36 +1,32 @@
-#TEMP path to log file
-file_path = r"Sample Log\samaple_log1.txt"
+def file_path():
+    #temp for open file explorer
+    #TEMP path to log file
+    return r"Sample Log\samaple_log1.txt"
 
-file = open(file_path, "r")
-line = file.readline()
+#log level count
+def log_analysis(path):
+    count_info = 0
+    count_error = 0
+    count_warning = 0
 
-#counting the amount of each log level
-def log_levels_count(content, info, error, warning):
-    if "INFO" in content:
-        info += 1
-    elif "ERROR" in content:
-        error += 1
-    elif "WARNING" in content:
-        warning += 1
+    try:
+        with open(path) as file:
+            for line in file:
+
+                content = line.upper()
+                if "INFO" in content:
+                    count_info += 1
+                elif "ERROR" in content:
+                    count_error += 1
+                elif "WARNING" in content:
+                    count_warning += 1
+
+    except FileNotFoundError:
+        print(f"Error: The file '{file_path}' was not found.")
+        return None, None, None, None
     
-    return info, error, warning
-
-count_info = 0
-count_error = 0
-count_warning = 0
-
-while line:
-    print(line.strip())
-    count_info, count_error, count_warning = log_levels_count(line, count_info, count_error, count_warning)
-    line = file.readline()
-
-total_log = count_info + count_error + count_warning
-
-print("--- Log Analysis Report ---")
-print(f"Total Log: {total_log}")
-print(f"Info: {count_info}")
-print(f"Error: {count_error}")
-print(f"Warning: {count_warning}")
+    total_log = count_info + count_error + count_warning
+    return total_log, count_info, count_error, count_warning
 
 while True:
     print("\nMain Menu:")
@@ -41,7 +37,18 @@ while True:
     choice = input("Enter your choice: ")
 
     if choice == '1':
-        print("Test 1")
+        print("--- Log Analysis Report ---")
+        path = file_path()
+        total_log, info, error, warning = log_analysis(path)
+
+        if total_log is not None:
+            print(f"Total Log: {total_log}")
+            print(f"Info: {info}")
+            print(f"Error: {error}")
+            print(f"Warning: {warning}")
+        
+        else:
+            print("Error reading log")
     
     elif choice == '2':
         print("Test 2")
