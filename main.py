021613bd35894be3ea08d854ff0d19045319cdbf
NoxSol9510 <114277558+NoxSol9510@users.noreply.github.com>
2025-08-20 14:59:29 +0700

    Add a simple menu

--- main.py
+++ main.py
@@ -24,6 +24,31 @@ while line:
     count_info, count_error, count_warning = log_levels_count(line, count_info, count_error, count_warning)
     line = file.readline()
 
+total_log = count_info + count_error + count_warning
+
+print("--- Log Analysis Report ---")
+print(f"Total Log: {total_log}")
 print(f"Info: {count_info}")
 print(f"Error: {count_error}")
 print(f"Warning: {count_warning}")
+
+while True:
+    print("\nMain Menu:")
+    print("1. Log Analysis Report")
+    print("2. Search Keyword")
+    print("0. Exit")
+
+    choice = input("Enter your choice: ")
+
+    if choice == '1':
+        print("Test 1")
+    
+    elif choice == '2':
+        print("Test 2")
+    
+    elif choice == '0':
+        print("Exiting the program.")
+        break
+
+    else:
+        print("Invalid input")
