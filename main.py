149157a8bea3a60520f8a83c6813045c4b6e421a
NoxSol9510 <114277558+NoxSol9510@users.noreply.github.com>
2025-08-22 23:12:31 +0700

    Add export option

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ while True:
     print("\nMain Menu:")
     print("1. Log Analysis Report")
     print("2. Search Keyword")
+    print("3. Export File")
     print("0. Exit")
 
     choice = input("Enter your choice: ")
@@ -43,6 +44,30 @@ while True:
         for item in list_match:
             print(item)
     
+    elif choice == '3':
+        path = file_path()
+
+        print("1. txt")
+        print("2. csv")
+        print("3. Cancel")
+
+        while True:
+            file_type = input("Choose file type: ")
+
+            if file_type == '1':
+                log_export(path, 1)
+                break
+            
+            elif file_type == '2':
+                log_export(path, 2)
+                break
+            
+            elif file_type == '3':
+                break
+
+            else:
+                print("Invalid input")
+
     elif choice == '0':
         print("Exiting the program.")
         break
